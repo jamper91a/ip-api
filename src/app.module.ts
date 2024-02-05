f25f749ed4b0ba564bdaace88144451eccb03fc4
jorge <jorge@lifereader.com>
2024-02-05 18:23:01 +1300

    Implementing class validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,6 +12,7 @@ import { PrismaService } from '../prisma/prisma.service';
       envFilePath: `./config/env/${process.env.NODE_ENV}.env`,
       load: [configuration],
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get('port');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
