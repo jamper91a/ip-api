d67fc4046491fb08d01fbfec5e120ba124102dc9
jorge <jorge@lifereader.com>
2024-02-08 17:40:17 +1300

    Clean code

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -1,9 +1,14 @@
-import {CanActivate, ExecutionContext, Injectable, UnauthorizedException,} from '@nestjs/common';
-import {JwtService} from '@nestjs/jwt';
-import {jwtConstants} from './constants';
-import {Request} from 'express';
-import {IS_PUBLIC_KEY} from '../../decorators/public.decorator';
-import {Reflector} from '@nestjs/core';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../../decorators/public.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
