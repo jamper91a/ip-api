56da2d3aaef4f4017442eef1e195b5dafef673b0
jorge <jorge@lifereader.com>
2024-02-08 17:04:07 +1300

    Implementing helmet

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as compression from 'compression';
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,6 +16,7 @@ async function bootstrap() {
     }),
   );
   app.use(compression());
+  app.use(helmet());
   await app.listen(port);
 }
 bootstrap();
