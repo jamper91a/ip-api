b722112be7a0a8efadaabceb871b55aa96409ab1
jorge <jorge@lifereader.com>
2024-02-09 19:25:26 +1300

    Cleaning code

--- src/modules/api-usages/api-usages.service.ts
+++ src/modules/api-usages/api-usages.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateApiUsageDto } from './dto/create-api-usage.dto';
-import { UpdateApiUsageDto } from './dto/update-api-usage.dto';
 
 @Injectable()
-export class ApiUsagesService {
-  create(createApiUsageDto: CreateApiUsageDto) {
-    return 'This action adds a new apiUsage';
-  }
-
-  findAll() {
-    return `This action returns all apiUsages`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} apiUsage`;
-  }
-
-  update(id: number, updateApiUsageDto: UpdateApiUsageDto) {
-    return `This action updates a #${id} apiUsage`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} apiUsage`;
-  }
-}
+export class ApiUsagesService {}
--- src/modules/ip-data/ip-data.service.ts
+++ src/modules/ip-data/ip-data.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateIpDatumDto } from './dto/create-ip-datum.dto';
-import { UpdateIpDatumDto } from './dto/update-ip-datum.dto';
 
 @Injectable()
-export class IpDataService {
-  create(createIpDatumDto: CreateIpDatumDto) {
-    return 'This action adds a new ipDatum';
-  }
-
-  findAll() {
-    return `This action returns all ipData`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} ipDatum`;
-  }
-
-  update(id: number, updateIpDatumDto: UpdateIpDatumDto) {
-    return `This action updates a #${id} ipDatum`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} ipDatum`;
-  }
-}
+export class IpDataService {}
